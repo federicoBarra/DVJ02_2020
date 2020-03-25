ebd098221b13a6c8284e4f68b1fadcc62dd09571
federico_barra <federico.n.barra@gmail.com>
2020-03-24 21:59:11 -0300

    Clase 03

---
+++ DVJ02 2020/Assets/Clases/Clase 03/Ejemplos/InputBasico.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DVJ02.Clase03
+{
+public class InputBasico : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Debug.Log("Alpha1 presionado");
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Barra espaciadora soltada");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Valor Horizontal al mantener A: " + horizontal);
+            Debug.Log("Valor Vertical al mantener A: " + vertical);
+        }
+    }
+}
+}
