02791afba06b9e1bbb8add06d648c2ee67db18e6
federico_barra <federico.n.barra@gmail.com>
2020-06-22 13:18:55 -0300

    Clase 13

---
+++ DVJ02 2020/Assets/Clases/Clase 13/Ejemplos/09_Parallax/BackgroundParallax.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace DVJ02.Clase13
+{
+	public class BackgroundParallax : MonoBehaviour
+	{
+		[System.Serializable]
+		public class ParallaxLayer
+		{
+			public GameObject layer;
+			public float moveCameraRatio = 0;
+			public float moveCameraRatioY = 0;
+		}
+
+		public ParallaxLayer[] layers;
+
+		float lastCamXPos;
+
+		void LateUpdate()
+		{
+			float mainCamPos = Camera.main.transform.position.x;
+			float deltaCamX = lastCamXPos - mainCamPos;
+
+			foreach (ParallaxLayer layer in layers)
+			{
+				layer.layer.transform.position += Vector3.right * deltaCamX * layer.moveCameraRatio;
+				layer.layer.transform.position += Vector3.up * deltaCamX * layer.moveCameraRatioY;
+			}
+
+			lastCamXPos = mainCamPos;
+		}
+	}
+}
---
+++ DVJ02 2020/Assets/Clases/Clase 13/Ejemplos/09_Parallax/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DVJ02.Clase13
+{
+	public class CameraFollow : MonoBehaviour
+	{
+		public Transform target;
+		public Vector3 distance;
+		public float camSpeed = 10;
+		public AnimationCurve bulletTime;
+		private void LateUpdate()
+		{
+			transform.position = Vector3.Slerp(transform.position, target.position + distance, camSpeed * Time.deltaTime);
+
+			Time.timeScale = 0;
+			//transform.position = target.position + distance;
+		}
+
+		//corutina
+		//Time.timeScale = bulletTime.Evaluate(t);
+
+	}
+}
